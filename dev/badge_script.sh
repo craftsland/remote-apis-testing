a239a39f69b534604cf0cc64d1cef2aa9dfb1a7a
Scott Clarke <scott.clarke@codethink.co.uk>
2019-11-18 16:15:00 +0000

    .gitlab-ci.yml: Refactor pages script
    Remove the messy block of if statements and replace them with a call
    to a script (dev/badge_script.sh) which should be easier to maintain

---
+++ dev/badge_script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+  echo "Expected BADGE_URL"
+  echo "Usage: badge_script.sh BADGE_URL"
+  exit 1
+fi
+
+get_badge() {
+  if [ ! -f "badges/${SERVER}-${CLIENT}-deployed.svg" ]; then
+    curl -o badges/${SERVER}-${CLIENT}-deployed.svg $BADGE_URL/${SERVER}-failed-red.svg
+  fi
+}
+
+BADGE_URL=$1
+
+# Bazel
+CLIENT=bazel
+for SERVER in buildbarn buildfarm buildgrid
+do
+  get_badge
+done
+
+# Buildstream
+CLIENT=buildstream
+for SERVER in buildgrid
+do
+  get_badge
+done
+
+# recc
+CLIENT=recc
+for SERVER in buildgrid
+do
+  get_badge
+done
+
+# Times
+for SERVER in buildbarn buildfarm buildgrid
+do
+  if [ ! -f "badges/${SERVER}-time.svg" ]; then
+    curl -o badges/${SERVER}-time.svg $BADGE_URL/${SERVER}_bazel-failed-red.svg
+  fi
+done
+
+# Concurrency-1 times
+for SERVER in buildbarn buildfarm
+do
+  if [ ! -f "badges/${SERVER}-concurrency-1-time.svg" ]; then
+    curl -o badges/${SERVER}-concurrency-1-time.svg $BADGE_URL/${SERVER}_bazel-failed-red.svg
+  fi
+done
