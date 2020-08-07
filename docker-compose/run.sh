ddd2d0feea7c15abc3b8cad9224ede42e8380aad
Christopher Phang <christopher.phang@codethink.co.uk>
2020-08-07 14:06:08 +0100

    Add buildfarm/bazel testing with docker-compose
    This adds the requisite docker-compose files for deploying
    buildfarm, and any necessary configuration.
    
    In addition, to make the bazel client interchangeable
    between server implementations, the buildbarn configuration
    is tweaked somewhat to eliminate the dependency on the
    rbe 16.04 container. The run.sh script is also made
    more configurable so that it can be used for more than
    just buildbarn and bazel.
    
    Gitlab-ci.yml jobs are added for buildfarm and bazel.

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,15 +1,40 @@
-\# The contents of this file are covered by APACHE License Version 2.
+# The contents of this file are covered by APACHE License Version 2.
 # See licenses/APACHEV2-LICENSE.txt for more information.
+#
+# Runs docker-compose files for server and client REAPI implementations
 
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -eux
+set -eu
 
-worker="worker-ubuntu16-04"
+while getopts ":s:c:" opt; do
+  case ${opt} in
+    s ) SERVER="$OPTARG";;
+    c ) CLIENT="$OPTARG";;
+    : ) echo "Missing argument for -$OPTARG" && exit 1;;
+    \?) echo "./run.sh -s [server] -c [client]" && exit 1;;
+  esac
+done
 
+worker="worker"
+
+rm -rf worker
 mkdir -m 0777 "${worker}" "${worker}/build"
 mkdir -m 0700 "${worker}/cache"
 
-docker-compose -f docker-compose_buildbarn.yml up -d 
-docker-compose -f docker-compose_buildbarn.yml logs --follow &
-docker-compose -f docker-compose-bazel.yml up --exit-code-from bazel-client
+cleanup() {
+    EXIT_STATUS=$?
+    docker-compose -f $SERVER down 
+    docker-compose -f $CLIENT down 
+    exit $EXIT_STATUS
+}
+trap cleanup EXIT
+
+docker-compose -f $SERVER build
+docker-compose -f $SERVER up -d
+docker-compose -f $SERVER logs --follow &
+
+docker-compose -f $CLIENT build
+docker-compose -f $CLIENT up --exit-code-from client
+
+
